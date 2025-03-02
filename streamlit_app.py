fcc9d42902f4418b7ae106225218afc51d876a01
igichan <igichan52@gmail.com>
2025-03-02 19:56:35 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ timetable_template = {
 }
 
 # Streamlit UI 생성
-st.title("📅 학교 시간표 생성")
+st.title("팔마고 3학년 시간표 생성")
 
 # 반 선택
 class_number = st.selectbox("반을 선택하세요", list(timetable_template.keys()))
